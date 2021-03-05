d6af0d0058765f4030f47c559ca2e031bc2ea055
mlpeck <mlpeck@arkbluecross.com>
2021-03-04 20:58:16 -0600

    adding the base files

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Portfolio from '../views/Portfolio.vue';
+import Movies from '../views/Movies.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/movies',
+    name: 'Movies',
+    component: Movies
   }
 ];
 
