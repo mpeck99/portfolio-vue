780aa006e7bef785ecb8a82f7c4274d1f5833e2c
mlpeck <mlpeck@arkbluecross.com>
2021-03-07 21:55:01 -0600

    styling the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,3 +15,48 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+.content-home {
+  margin: auto 0;
+  font-family: $main;
+
+  h1 {
+    margin-bottom: 0;
+
+    font-size: 2rem;
+    color: $navy;
+
+    letter-spacing: 0.25rem;
+    line-height: 2;
+
+    @include tablet {
+      font-size: 5rem;
+    }
+  }
+
+  p {
+    margin: 0;
+
+    text-align: center;
+    font-weight: 400;
+    font-size: 1.5rem;
+    font-family: $secondary;
+    color: $navy;
+
+    @include tablet {
+      font-size: 3rem;
+    }
+  }
+}
+
+.layout--bg-img {
+  background-image: linear-gradient(
+      rgba($navy, 1),
+      rgba($turquoise, 0.75),
+      rgba($navy, 1)
+    ),
+    url('../assets/images/home-background.jpg');
+  background-size: cover;
+  background-position: center;
+}
+</style>
